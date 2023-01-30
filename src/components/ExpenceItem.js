adcf60739dd470ea84743314f9695daebe6a73ef
chagan singh <chagansingh9876@gmail.com>
2023-01-30 12:19:08 +0530

    adding components

---
+++ src/components/ExpenceItem.js
@@ -0,0 +1,12 @@
+function ExpenceItem () {
+    return (
+      <div>
+        <div>March 12 2021</div>
+        <div>
+          <h2>Car insurance</h2>
+          <div>$294</div>
+        </div>
+      </div>
+    );
+}
+export default ExpenceItem;
