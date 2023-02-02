d3ffe8458d41110bda74ece3ffdbdcd2a243aa27
chagansingh <chagansingh9876@gmail.com>
2023-02-02 12:15:20 +0530

    new way conditionals

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,11 @@
 
 import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
+
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -21,28 +22,7 @@ const Expenses = (props) => {
     <div>
       <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {filterExpenses.length === 0 ? (
-        <p>No Expense Found</p>
-        ):(filterExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-          ))
-        )}
-        {filterExpenses.length === 1 ? (
-        <p>There is Just ONE Add More Elements...</p>
-        ):(filterExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-          ))
-        )}
+        <ExpensesList items={filterExpenses}/>
       </Card>
     </div>
   );
